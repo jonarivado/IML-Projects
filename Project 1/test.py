bae115aab403da5f9fd843f2ed88d3cfe7a1f048
glahni <mglahn@ethz.ch>
2022-03-18 12:51:33 +0100

    First try task 1.a

---
+++ Project 1/test.py
@@ -0,0 +1,16 @@
+# from sklearn.linear_model import Ridge
+# import numpy as np
+# n_samples, n_features = 10, 5
+# rng = np.random.RandomState(0)
+# y = rng.randn(n_samples)
+# X = rng.randn(n_samples, n_features)
+# clf = Ridge(alpha=1.0)
+# clf.fit(X, y)
+# Ridge()
+# print(clf.score(X, y))
+import numpy as np
+arr=np.empty(3)
+arr[0]=3
+arr[1]=4
+
+print(arr)
