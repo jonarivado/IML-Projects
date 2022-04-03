39b0e3d72ec1489d4368993eb96cb59abf2b3f4c
glahni <mglahn@ethz.ch>
2022-04-03 10:40:12 +0200

    init project 2

--- Project 1.a)/main.py
+++ Project 1.a)/main.py
@@ -1,24 +1,27 @@
 import numpy as np
 import pandas as pd
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, Lasso
 # load the dataset
 train_df=pd.read_csv('train.csv')
 y=pd.DataFrame(train_df['y'])
 X=train_df.drop(labels='y',axis=1)
 
-lambdas=[0.1,1,10,100,200]
-score_array=np.empty(5)
+lambdas = [0.1, 1, 10, 100, 200]
+avg_acc = np.zeros(5)
+
 for i in range(5):
-    model = Ridge(alpha=lambdas[i])
-    # define model evaluation method
-    cv = RepeatedKFold(n_splits=10, n_repeats=5, random_state=1)
-    # evaluate model
-    scores = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv=cv, n_jobs=-1)
-    # force scores to be positive
-    scores = np.absolute(scores)
-    score_array[i] =np.mean(scores)
-print(score_array)
-score_df = pd.DataFrame(score_array)
+
+    kf = RepeatedKFold(n_splits=10, n_repeats=10,random_state=42)
+    
+    model = Ridge(alpha=[lambdas[i]], max_iter=10000, tol=0.00001)
+
+    result = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv = kf)
+    result = np.absolute(result)
+    avg_acc[i] = np.mean(result)
+    print("Average accuracy: {}".format(result.mean()))
+
+score_df = pd.DataFrame(avg_acc)
 score_df.to_csv('submission.csv', index=False, header= None)
--- Project 1.a)/main2nd.py
+++ Project 1.a)/main2nd.py
@@ -1,12 +1,28 @@
-from sklearn.linear_model import RidgeCV
 import pandas as pd
-
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+from sklearn.linear_model import LassoCV, Ridge
+from sklearn.metrics import mean_squared_error
 
 #load dataset
 train_df=pd.read_csv(r'C:\Users\Manue\Documents\ETH\Master\2.semester\introduction to machinelearning\code projects\1.a\train.csv')
 y=pd.DataFrame(train_df['y'])
 X=train_df.drop(labels='y',axis=1)
 
-#ridge regression
-clf = RidgeCV(alphas=[0.1] , cv=10).fit(X, y)
-print(clf.score(X, y))
+lambdas = [0.1, 1, 10, 100, 200]
+avg_acc = np.zeros(5)
+
+for i in range(5):
+
+    kf = RepeatedKFold(n_splits=10, n_repeats=10, random_state=0)
+    
+    model = Ridge(alpha=[lambdas[i]], max_iter=10000, tol=0.00001)
+
+    result = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv = kf)
+    result = np.absolute(result)
+    avg_acc[i] = np.mean(result)
+    print("Average accuracy: {}".format(result.mean()))
+
+RMSE = pd.DataFrame(avg_acc)
+RMSE.to_csv('submission.csv', index=False, header= None)
